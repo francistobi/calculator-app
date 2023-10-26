fe4b2d250d45dbd9d9eda76043c8edf20b84ff02
francistobi <frobigwe305@gmail.com>
2023-10-26 19:46:54 +0100

    new commit

---
+++ run.js
@@ -0,0 +1,21 @@
+
+      let displayValue = "";
+
+      function clearDisplay() {
+        displayValue = "";
+        document.getElementById("display").value = displayValue;
+      }
+
+      function addToDisplay(value) {
+        displayValue += value;
+        document.getElementById("display").value = displayValue;
+      }
+
+      function calculate() {
+        try {
+          displayValue = eval(displayValue);
+          document.getElementById("display").value = displayValue;
+        } catch (error) {
+          document.getElementById("display").value = "Error";
+        }
+    }
